c1d83816aaade984edbea13d27d65c86c2566b7d
Sharon Wong <26sswong@gmail.com>
2021-08-26 16:49:23 -0700

    add backdrop only view for popular movies

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Flixster
- Android University: Assignment 1
+# Assignment 1 - *Flixster*
+
+Flixster is an android application that allows users to browse a list of movies currently playing in theaters.
+
+Submitted by: Sharon Wong
+
+Time spent: 6 hours spent in total
+
+## User Stories
+
+The following **required** features are completed:
+[x] User can view a list of movies (title, poster image, and overview) currently playing in theaters from the Movie Database API.
+
+The following **optional** features are implemented:
+[x] Views should be responsive for both landscape/portrait mode.
+- In portrait mode, the poster image, title, and movie overview is shown.
+- In landscape mode, the rotated alternate layout should use the backdrop image instead and show the title and movie overview to the right of it.
+[x] Display a nice default placeholder graphic for each image during loading.
+[x] Improve the user interface through styling and coloring
+[x] * For popular movies (i.e. a movie voted 8 stars or more), the full backdrop image is displayed. Otherwise, a poster image, the movie title, and overview is listed.
--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -18,7 +18,7 @@ import com.example.flixster.models.Movie;
 
 import java.util.List;
 
-public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+public class MovieAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     Context context;
     List<Movie> movies;
@@ -42,31 +42,60 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     // inflates a layout from XML and returning the holder
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Log.d("MovieAdapter", "onCreateViewHolder");
-        ViewHolder viewHolder;
+        RecyclerView.ViewHolder viewHolder;
         LayoutInflater inflater = LayoutInflater.from(context);
 
         if (viewType == POPULAR) {
             View v2 = inflater.inflate(R.layout.item_movie2, parent, false);
-            viewHolder = new ViewHolder(v2);
+            viewHolder = new ViewHolder2(v2);
         }
         else {
             View v1 = inflater.inflate(R.layout.item_movie1, parent, false);
-            viewHolder = new ViewHolder(v1);
+            viewHolder = new ViewHolder1(v1);
         }
 
         return viewHolder;
     }
 
-    // populating data into the item through holder
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        // get the movie at the position
-        Movie movie = movies.get(position);
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder.getItemViewType() == POPULAR) {
+            ViewHolder2 vh2 = (ViewHolder2) holder;
+            configureViewHolder2(vh2, position);
+        }
+        else {
+            ViewHolder1 vh1 = (ViewHolder1) holder;
+            configureViewHolder1(vh1, position);
+        }
+    }
 
-        // bind movie data into the view holder
-        holder.bind(movie);
+    private void configureViewHolder1(ViewHolder1 vh1, int position) {
+        Movie movie = movies.get(position);
+        if (movie != null) {
+            vh1.getTvTitle().setText(movie.getTitle());
+            vh1.getTvOverview().setText(movie.getOverview());
+            String imageUrl;
+
+            // if phone in landscape, set image to backdrop
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE
+                    || movie.getRating() >= 8) {
+                imageUrl = movie.getBackdropPath();
+            }
+            else {
+                imageUrl = movie.getPosterPath();
+            }
+
+            Glide.with(context).load(imageUrl).placeholder(R.drawable.ic_baseline_movie_24).into(vh1.getIvPoster());
+        }
+    }
+
+    private void configureViewHolder2(ViewHolder2 vh2, int position) {
+        Movie movie = movies.get(position);
+        String imageUrl = movie.getBackdropPath();
+
+        Glide.with(context).load(imageUrl).placeholder(R.drawable.ic_baseline_movie_24).into(vh2.getIvPoster());
     }
 
     // return total count of items in the list
@@ -75,35 +104,57 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         return movies.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder1 extends RecyclerView.ViewHolder {
+        private TextView tvTitle;
+        private TextView tvOverview;
+        private ImageView ivPoster;
 
-        TextView tvTitle;
-        TextView tvOverview;
-        ImageView ivPoster;
+        public TextView getTvTitle() {
+            return tvTitle;
+        }
 
-        public ViewHolder(@NonNull View itemView) {
+        public void setTvTitle(TextView tvTitle) {
+            this.tvTitle = tvTitle;
+        }
+
+        public TextView getTvOverview() {
+            return tvOverview;
+        }
+
+        public void setTvOverview(TextView tvOverview) {
+            this.tvOverview = tvOverview;
+        }
+
+        public ImageView getIvPoster() {
+            return ivPoster;
+        }
+
+        public void setIvPoster(ImageView ivPoster) {
+            this.ivPoster = ivPoster;
+        }
+
+        public ViewHolder1(@NonNull View itemView) {
             super(itemView);
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
         }
+    }
 
-        public void bind(Movie movie) {
-            tvTitle.setText(movie.getTitle());
-            tvOverview.setText(movie.getOverview());
-            String imageUrl;
+    public class ViewHolder2 extends RecyclerView.ViewHolder {
+        private ImageView ivPoster;
 
-            // if phone in landscape, set image to backdrop
-            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE
-                || movie.getRating() >= 8) {
-                imageUrl = movie.getBackdropPath();
-            }
-            else {
-                // else if portrait, set image to poster
-                imageUrl = movie.getPosterPath();
-            }
+        public ImageView getIvPoster() {
+            return ivPoster;
+        }
 
-            Glide.with(context).load(imageUrl).placeholder(R.drawable.ic_baseline_movie_24).into(ivPoster);
+        public void setIvPoster(ImageView ivPoster) {
+            this.ivPoster = ivPoster;
+        }
+
+        public ViewHolder2(@NonNull View itemView) {
+            super(itemView);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
         }
     }
 }
